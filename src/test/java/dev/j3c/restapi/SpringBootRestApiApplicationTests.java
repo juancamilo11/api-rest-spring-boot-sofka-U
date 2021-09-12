77722a948fd45bca2bb02d22bd83a2c42b6e310b
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-12 01:17:09 -0500

    Inicia el proyecto

---
+++ src/test/java/dev/j3c/restapi/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
