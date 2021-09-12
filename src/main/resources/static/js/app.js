ac3940d59e6b1dce9383ddbec4d22c1d42cf2450
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-12 14:06:33 -0500

    Se agregó la función Fetch para eliminar una persona

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,96 +7,6 @@ const $tableBody = document.querySelector('.users-table__body');
 const $tableTemplate = document.getElementById('tbody-template').content;
 const $tableFragment = document.createDocumentFragment();
 
-
-const ingresarOActualizar = function(options) {
-    const {method, url, success, error, data} = options;
-    fetch(url,{
-        method,
-        headers: {
-            "content-type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify({
-            nombre: data.nombre,
-            apellido: data.apellido,
-            edad: data.edad,
-            telefono: data.telefono,
-            genero: data.genero
-        })
-    })
-    .then(res => {
-        if(res.ok) {
-            success(res.json());
-        } else {
-            Promise.reject("Se ha producido un error al intentar ingresar el nuevo usuario");
-        }
-    })
-    .catch(err => {
-        error(err);
-    })
-}
-
-const ingresarNuevaPersona = function(id,nombre,apellido,edad,telefono,genero) {
-    ingresarOActualizar({
-        method:'POST',
-        url: 'http://localhost:8080/api/persona/guardar',
-        success: function(data) {
-            alert(`El usuario se ha creado correctamente!`);
-            location.reload();
-        },
-        error: function(mensaje) {
-            $table.insertAdjacentElement('afterend',`<p><b>${mensaje}</b></p>`);
-        },
-        data: {
-            nombre,
-            apellido,
-            edad,
-            telefono,
-            genero
-        }
-    });
-}
-
-const actualizarPersona = function(id,nombre,apellido,edad,telefono,genero){
-    ingresarOActualizar({
-        method:'PUT',
-        url: `http://localhost:8080/api/persona/${id}`,
-        success: function(data) {
-            alert(`El usuario se ha actualizado correctamente!`);
-            location.reload();
-        },
-        error: function(mensaje) {
-            $table.insertAdjacentElement('afterend',`<p><b>${mensaje}</b></p>`);
-        },
-        data: {
-            nombre,
-            apellido,
-            edad,
-            telefono,
-            genero
-        }
-    });
-}
-
-document.addEventListener('submit',function (e){
-    if(e.target === $form) {
-        e.preventDefault();
-        const id = $form.id.value,
-            nombre = $form.nombre.value,
-            apellido = $form.apellido.value,
-            edad = $form.edad.value,
-            telefono = $form.telefono.value,
-            genero = $form.genero.value;
-        if(!id) {
-            //Operación --> Guardar una nueva persona
-            ingresarNuevaPersona(id,nombre,apellido,edad,telefono,genero);
-        } else {
-            //Operación --> UPDATE
-            actualizarPersona(id,nombre,apellido,edad,telefono,genero);
-        }
-    }
-});
-
-
 const obtenerPersonas = async function(){
     try{
         let res = await fetch("http://localhost:8080/api/persona/listar"),
@@ -135,6 +45,129 @@ const obtenerPersonas = async function(){
     }
 }
 
+const FetchingresarOActualizar = function(options) {
+    const {method, url, success, error, data} = options;
+    fetch(url,{
+        method,
+        headers: {
+            "content-type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            nombre: data.nombre,
+            apellido: data.apellido,
+            edad: data.edad,
+            telefono: data.telefono,
+            genero: data.genero
+        })
+    })
+    .then(res => {
+        if(res.ok) {
+            success(res.json());
+        } else {
+            Promise.reject("Se ha producido un error al intentar ingresar el nuevo usuario");
+        }
+    })
+    .catch(err => {
+        error(err);
+    })
+}
+
+const ingresarNuevaPersona = function(id,nombre,apellido,edad,telefono,genero) {
+    FetchingresarOActualizar({
+        method:'POST',
+        url: 'http://localhost:8080/api/persona/guardar',
+        success: function(data) {
+            alert(`El usuario se ha creado correctamente!`);
+            location.reload();
+        },
+        error: function(mensaje) {
+            $table.insertAdjacentElement('afterend',`<p><b>${mensaje}</b></p>`);
+        },
+        data: {
+            nombre,
+            apellido,
+            edad,
+            telefono,
+            genero
+        }
+    });
+}
+
+const actualizarPersona = function(id,nombre,apellido,edad,telefono,genero){
+    FetchingresarOActualizar({
+        method:'PUT',
+        url: `http://localhost:8080/api/persona/${id}`,
+        success: function(data) {
+            alert(`El usuario se ha actualizado correctamente!`);
+            location.reload();
+        },
+        error: function(mensaje) {
+            $table.insertAdjacentElement('afterend',`<p><b>${mensaje}</b></p>`);
+        },
+        data: {
+            nombre,
+            apellido,
+            edad,
+            telefono,
+            genero
+        }
+    });
+}
+
+const eliminarPersona = async function(id) {
+    try{
+        let res = await fetch(`http://localhost:8080/api/persona/${id}`, {method:"DELETE"});
+        if(!res.ok) {
+            throw {status: res.status , statusText: res.statusText}
+        }
+        location.reload();
+    } catch(error) {
+        let mensaje = error.statusText || "Ha ocurrido un error";
+        alert(`Error al intentar eliminar la persona: ${error}: ${mensaje}`);
+    }
+}
+
+document.addEventListener('submit',function (e){
+    if(e.target === $form) {
+        e.preventDefault();
+        const id = $form.id.value,
+            nombre = $form.nombre.value,
+            apellido = $form.apellido.value,
+            edad = $form.edad.value,
+            telefono = $form.telefono.value,
+            genero = $form.genero.value;
+        if(!id) {
+            //Operación --> Guardar una nueva persona
+            ingresarNuevaPersona(id,nombre,apellido,edad,telefono,genero);
+        } else {
+            //Operación --> Actualizar una persona
+            actualizarPersona(id,nombre,apellido,edad,telefono,genero);
+        }
+    }
+});
+
+document.addEventListener('click',(event) => {
+    if(event.target.matches('.btn-editar')) {
+        const $btnIngreso = document.getElementById('btn-ingreso');
+        const $btnEditPresionado = event.target;
+        $btnIngreso.value = "Editar Usuario";
+
+        $form.id.value = $btnEditPresionado.dataset.id;
+        $form.nombre.value = $btnEditPresionado.dataset.nombre;
+        $form.apellido.value = $btnEditPresionado.dataset.apellido;
+        $form.edad.value = $btnEditPresionado.dataset.edad;
+        $form.telefono.value = $btnEditPresionado.dataset.telefono;
+        $form.genero.value = $btnEditPresionado.dataset.genero;
+
+    } else if(event.target.matches('.btn-eliminar')) {
+        const $btnElimPresionado = event.target,
+            id = $btnElimPresionado.dataset.id;
+        let confirmacion = window.confirm(`¿Desea eliminar al usuario con id: ${id}?`);
+        if(confirmacion) {
+            eliminarPersona(id);
+        }
+    }
+});
 
 window.addEventListener('load', obtenerPersonas);
 
