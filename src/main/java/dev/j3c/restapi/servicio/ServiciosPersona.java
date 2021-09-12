8a2fc228f33e16b3aa4b6b04056b50bc9b0164f9
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-12 02:31:13 -0500

    Se completó la operación listar

--- src/main/java/dev/j3c/restapi/servicio/ServiciosPersona.java
+++ src/main/java/dev/j3c/restapi/servicio/ServiciosPersona.java
@@ -1,15 +1,21 @@
 package dev.j3c.restapi.servicio;
 
 import dev.j3c.restapi.entidad.Persona;
+import dev.j3c.restapi.repositorio.InterfazRepositorioPersona;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ServiciosPersona implements InterfazServiciosPersona{
+
+    @Autowired
+    private InterfazRepositorioPersona data;
+
     @Override
     public List<Persona> listar() {
-        return null;
+        return (List<Persona>) data.findAll();
     }
 
     @Override
